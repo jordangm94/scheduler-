f5d29d28f07f22b9f5b4a812d0034acf1faabc85
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-01 17:18:32 -0400

    Added classnames functions to add conditionals that determine appropriate css class name based on if label is selected, if spots are full etc. Also created test item to ensure that spots component was working correctly. Lastly created a format spots function in DayList Item component to handle appearance of component if available spots is 0, 1, or more

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,4 +1,6 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss"
 
 //First attempt at creating the DayListItem
 // export default function DayListItem(props) {
@@ -16,12 +18,25 @@ import React from "react";
   //Note: for onClick, we need to apply setDay so that it resets the day to the name of the one we clicked on. 
   //Since we need to pass props.name to the setDay function, we must put it in anonymous function in order for it to work
   //in react.
-  
+
 export default function DayListItem(props) {
+
+  const formatSpots = function(propSpots) {
+    if (props.spots === 0) {
+      return "no spots";
+    } else if (props.spots === 1) {
+      return "1 spot";
+    } else {
+      return props.spots + " spots";
+    }
+  }
+
+  let dayClass = classNames('day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': props.spots === 0 ? true: false})
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
