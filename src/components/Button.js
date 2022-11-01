1e2e445afcc024740d7b879ceac83486c0fed1c2
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-01 14:16:31 -0400

    Adjusted personal notes on button

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,17 +5,16 @@ import "components/Button.scss";
 import classNames from "classnames";
 
 export default function Button(props) {
-  // let buttonClass = "button";
-
-    //Now we use a library called class names above and in that we state the first name of class that will be stated indefinitely, 
+  //Now we use a library called class names which gives us the classNames function where we state the first name of the css class that will be stated indefinitely, 
   //then we add the conditionals in an object. In that object we make the css the key and the value the props.confirm or props.danger
 
-  //If props.confirm returns truthy, than the css key button--confirm is added to the button in the first parameter. 
+  //If props.confirm returns truthy, than the object key, which is css (button--confirm) is added to the button in the first parameter.
+  //Designating the class and allowing it to be targeted by class appropriately. 
   let buttonClass = classNames('button', {"button--confirm": props.confirm, "button--danger": props.danger })
   
   //Important note: This is how we handled the conditionals before, we checked if props.confirm is truthy, meaning if confirm prop
   //is actually passed, and if it is then we tack on button--confirm to what use to be the buttonClass = button. 
-
+  // let buttonClass = "button";
   // if (props.confirm) {
   //   buttonClass += " button--confirm";
   // }
