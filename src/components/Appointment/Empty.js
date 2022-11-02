bf789486230ea80c1796ad4b8db278d00dafc61d
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-02 16:52:44 -0400

    Added personal notes to empty

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function Empty(props) {
+  //Note we are calling onAdd function at end of image tag so that this event fires
+  //only when clicking on the + image, not the whole component! 
   return (
     <main className="appointment__add">
       <img className="appointment__add-button" src="images/add.png" alt="Add" onClick={props.onAdd} />
