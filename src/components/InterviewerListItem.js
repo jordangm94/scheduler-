101fbf5dbce31b12858719308496c62ae911931d
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-01 20:32:09 -0400

    Replaced hardcoded values in InterviwerListItem component with props passed down to it from stories in index.js

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-export default function InterviewerListItem() {
+import "components/InterviewerListItem.scss"
+
+export default function InterviewerListItem(props) {
 
   return (
     <li className="interviewers__item">
   <img
     className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
+    src={props.avatar}
     alt="Sylvia Palmer"
   />
-  Sylvia Palmer
+  {props.name}
 </li>
   )
 }
