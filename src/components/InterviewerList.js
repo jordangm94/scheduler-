3936d0d99ddaae77837c025dc6d1a33e4b3d6f9b
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-02 09:45:27 -0400

    Created new component called Interviewer List which will house multiple interviewerlistitem compnents, as well as created corresponding css file

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import InterviewerListItem from "./InterviewerListItem";
+
+import "components/InterviewerList.scss"
+
+export default function InterviewerList() {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  );
+}
