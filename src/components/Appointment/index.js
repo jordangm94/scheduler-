b1518c23a3ae6639e32153c899764a9193a3318c
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-03 09:48:25 -0400

    Created two new stories to have appointment component display the <Show> component when there is an interview booked and default to the <Empty> component when there are no bookings

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { Fragment } from 'react'
 
 import "components/Appointment/styles.scss"
+
+import Header from "./Header";
+
+import Empty from "./Empty";
+
+import Show from "./Show";
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+    </article>
   )
 }
