7a1329ae5d562a75e3e9b288e8352d55ed4b81ea
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-04 17:26:33 -0400

    Updated application.js with promise.all which will return multiple API requests, add a request for appointments and commented out hard coded data

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,44 +8,46 @@ import DayList from "./DayList";
 
 import Appointment from "./Appointment";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+import { getAppointmentsForDay } from "helpers/selectors";
+
+// const appointments = {
+//   "1": {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   "2": {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer:{
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   "3": {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   "4": {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer:{
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   "5": {
+//     id: 5,
+//     time: "4pm",
+//   }
+// };
 
 export default function Application(props) {
 
@@ -60,26 +62,26 @@ export default function Application(props) {
   //Function to setDay individually using setState. 
   const setDay = day => setState({ ...state, day });
 
-  //Function to setDays individually using setState
-  //Note here we must use prev to ensure that The action used to set the state still gets the previous state to ensure that only the days value changes.
-  const setDays = (days) => {setState(prev => ({ ...prev, days }))}
-
-
   //Trigger useEffect to make a request to /api/days using axios.get. Axios returns response in a promise and we update
   //the default days value by using setDays on the response.data(array of days objects)
   useEffect(() => {
-    Axios.get('/api/days')
-    .then(response => {setDays(response.data)
-      // How to set state with new format | setState({...state, day: 'Tuesday'})// setDays([...response.data])
-      // setState({...state})// setDays([...response.data])
-    });
-    //Note, we include an empty array as second argument for useEffect which tells it that we only run this api request once when the 
-    //component renders. This empty array, or state passed to this array tells useEffect what to do. If not added, will continuosly be triggered. 
+    Promise.all([
+      Axios.get('/api/days'),
+      Axios.get('/api/appointments')
+    ]).then((all) => {
+      console.log(all[0].data.name)
+      console.log(all[1].data);
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+    })
   }, [])
+  
+  //Note, we include an empty array as second argument for useEffect which tells it that we only run this api request once when the 
+  //component renders. This empty array, or state passed to this array tells useEffect what to do. If not added, will continuosly be triggered. 
+  
 
-  //With setDays updating our default days state, we will see that change on page within DayList Component that uses the days state
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const listOfAppointments = Object.values(appointments).map((appointmentObj) => (
+  const listOfAppointments = dailyAppointments.map((appointmentObj) => (
     <Appointment 
     key={appointmentObj.id}
     {...appointmentObj}
