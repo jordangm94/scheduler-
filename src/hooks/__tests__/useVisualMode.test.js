9bccd1baeee51dc8e25ae6f0b83aea145dfa9483
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-05 12:47:33 -0400

    Incorporated history state array to keep track of the modes we have been going through as well as back function to move back to previous mode in our history. Ensured back cannot go back past initial value. Also implemented corresponding tests

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -8,6 +8,8 @@ const FIRST = "FIRST";
 
 const SECOND = "SECOND";
 
+const THIRD = "THIRD";
+
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
@@ -19,4 +21,27 @@ test("useVisualMode should transition to another mode", () => {
 
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
+});
+
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
 });
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,13 +5,25 @@ const useVisualMode = function(initialMode) {
   //set the mode state with the initial mode provided
   const [mode, setMode] = useState(initialMode);
 
+  //Stateful array that stores history of modes
+  const [history, setHistory] = useState([initialMode]);
+
   //Transition function to take in newMode and set mode state to newMode
   const transition = function(newMode) {
     setMode(newMode)
   }
 
+  //Back function that allows for mode to be set back to previous mode in history array. Uses if conditional to ensure history.length to ensure that we are never backing past the fist value in array. 
+
+  //Needs to use setHistory as well in order for place in history to be remembered, that way if we call back again it will bring us back a spot from our current position, not the initial position. 
+  const back = function() {
+    if (history.length === 1) return 
+    setMode(history[history.length - 2])
+    setHistory(history.pop)
+  }
+
   //return an object with a mode property and transition function
-  return { mode, transition };
+  return { mode, transition, back };
 };
 
 export default useVisualMode;
