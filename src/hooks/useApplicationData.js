ee4924644317780ecb9b0b8d6ba92eb0e1493d06
Jordan Guerrero Martinez <jordan.guerrero.martinez@gmail.com>
2022-11-08 14:07:32 -0500

    Added days varable to bookinterview and cancelinterview functions, mapped over days to find matching appointment id, and if interview value was truthy or falsey. Adjusted spots value accordingly based on which function we are using, bookinterview or cancelinterview

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -16,8 +16,21 @@ const useApplicationData = function() {
   });
 
   function bookInterview(id, interview) {
+    //Declare variable called days and access state.days array and map over it, at each index there will be a day object with id, and appointments!
+    //We then will check if day.appointments includes the id of the id passed into function, if then we continue. We now check the appointments object 
+    //using the id t see if the interview value is falsey, if so it is free so we decrease spots by 1. 
+    const days = state.days.map( day => {
+      if (day.appointments.includes(id)) {
+        if (!state.appointments[id].interview) {
+          return {...day, spots: day.spots - 1}
+        }
+      }
+      return day
+    });
+
     //Create new appointment object, within this object we will access spread everything within specific appointments object,
     //which we targetted via ID. We also replace interview object within appointment, with new interview object coming in. 
+
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -28,16 +41,31 @@ const useApplicationData = function() {
       ...state.appointments,
       [id]: appointment
     };
+
+    
     //Next we are going to make an axios put request with the information we want to depsit, the appointment, it will follow this alias: 
     //axios.put(url, data). We make the Axios put request, and upon the response (which is a 204 status), we set the State to include the new appointment. 
     return Axios.put(`http://localhost:8001/api/appointments/${id}`, appointment)
-    .then(response => setState({...state, appointments})
+    .then(response => setState({...state, appointments, days})
     )
   }
 
   //This function will see interview object to null and make a delete request to the API to set interview by said ID to null. 
   //We pass this function to appointment, where we will use it as a prop in our function cancel
+  
   function cancelInterview(id) {
+    //Same as before, map through state.days array and gain access to each day object, check if id passed to function matches day appointments id,
+    //Once match found, check if appointments object has appointment with that id has interview that is full, if so we increment spots by 1,
+    //as this is what we will be deleting. 
+    const days = state.days.map( day => {
+      if (day.appointments.includes(id)) {
+        if (state.appointments[id].interview) {
+          return {...day, spots: day.spots + 1}
+        }
+      }
+      return day
+    });
+
     const appointment = {
       ...state.appointments[id],
       interview: null
@@ -47,7 +75,7 @@ const useApplicationData = function() {
       [id]: appointment
     }
     return Axios.delete(`http://localhost:8001/api/appointments/${id}`)
-    .then(response => setState({...state, appointments})
+    .then(response => setState({...state, appointments, days})
   )}
 
   //Function to setDay individually using setState. 
